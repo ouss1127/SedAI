c0ab69196246fef7c900e0d58b68d88d2c583c78
oussamaSeddini <oussamasdn1127@gmail.com>
2024-03-31 20:23:12 +0100

    Creat middle component

---
+++ src/components/Middle/Middle.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Middle.css'
+import { assets } from '../../assets/assets'
+
+const Middle = () => {
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>SedAI</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Dev</span></p>
+                <p>How I can help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Middle
