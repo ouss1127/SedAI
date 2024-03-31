f84faa4a23ca9668c2cf152eb6d8ed43163f2144
oussamaSeddini <oussamasdn1127@gmail.com>
2024-03-31 20:22:54 +0100

    Creat the structure of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Middle from './components/Middle/Middle'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Middle />
+    </>
+  )
+}
+
+export default App
