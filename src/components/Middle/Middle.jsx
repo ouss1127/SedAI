e0900663e30cacf04666da4923c278b8058865a7
oussamaSeddini <oussamasdn1127@gmail.com>
2024-04-01 18:48:12 +0100

    update middle component

--- src/components/Middle/Middle.jsx
+++ src/components/Middle/Middle.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Middle.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 const Middle = () => {
+
+const{onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
   return (
     <div className='main'>
         <div className="nav">
@@ -10,27 +14,63 @@ const Middle = () => {
             <img src={assets.user_icon} alt="" />
         </div>
         <div className="main-container">
-            <div className="greet">
-                <p><span>Hello, Dev</span></p>
-                <p>How I can help you today?</p>
+
+            {!showResult
+            ?<>
+                <div className="greet"> 
+                    <p><span>Hello, Dev</span></p>
+                    <p>How I can help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summrize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability og the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+            </>
+            : <div className="result">
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading
+                    ?<div className="loader">
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                    }
+                </div>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
+            }
+            
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input onChange={(e)=>setInput(e.target.value)} value={input} type='text' placeholder='Enter a prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+                    </div>
                 </div>
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
+                <p className='bottom-info'>
+                    Gemini may display inaccurate information, including about people. So check his answers.
+                </p>
             </div>
         </div>
     </div>
